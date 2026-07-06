e3e2dc0eb500b5ecb8d2b3952c6165fdfa5a3fa7
Ravi Kumar <ravikumaratri97@gmail.com>
2026-07-07 01:46:37 +0530

    new-changes

--- tests/validateDynamicButton.spec.ts
+++ tests/validateDynamicButton.spec.ts
@@ -6,6 +6,6 @@ test("vaildate dynamic button",async({page})=>{
 
   await page.goto("https://testautomationpractice.blogspot.com/");
   await page.locator("//button[@onclick='toggleButton(this)']").click();
-await page.waitForTimeout(3000);
+
 
 })
